37a710c7f5e79fededdcff735e5c683d78660b02
Daniil Domin <dyomin7499@gmail.com>
2025-08-21 20:08:49 +0300

    Created the project

---
+++ FriendsTestTask/FriendsTestTask/FriendsTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsTestTaskApp.swift
+//  FriendsTestTask
+//
+//  Created by ddomin on 21.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct FriendsTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
